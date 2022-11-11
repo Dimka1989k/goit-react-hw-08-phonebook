f211e1efa68cce8d51e6cf1477e403a1a521d81e
Dimon1989k <dima1989k@ukr.net>
2022-11-11 13:22:39 +0200

    finish

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react';
-import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid';
+import { useDispatch, useSelector } from 'react-redux';
+import { getContacts, addContact } from '../../redux/slice';
+
+// import PropTypes from 'prop-types';
 import './ContactForm.module.css';
 
-export const ContactForm = ({ onSubmit }) => {
+export const ContactForm = () => {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
+  const contacts = useSelector(getContacts);
+  const dispatch = useDispatch();
 
   const handleChange = event => {
-    console.log(event.target.value, event.target.name);
+    // console.log(event.target.value, event.target.name);
     const { name, value } = event.target;
     switch (name) {
       case 'name':
@@ -23,13 +29,26 @@ export const ContactForm = ({ onSubmit }) => {
 
   const handleSubmit = event => {
     event.preventDefault();
-    onSubmit(name, number);
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    const enterContacts = contacts.some(
+      contact =>
+        (contact.name === name.toLowerCase() && contact.number === number) ||
+        contact.number === number
+    );
+    enterContacts
+      ? alert(`${name} or ${number} is already in contacts`)
+      : dispatch(addContact(contact));
+
     setName('');
     setNumber('');
   };
 
   return (
-    
     <form onSubmit={handleSubmit}>
       <p>Name</p>
       <input
@@ -42,7 +61,7 @@ export const ContactForm = ({ onSubmit }) => {
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
       />
-      <p>Number</p>
+       <p>Number</p>
       <input
         type="tel"
         name="number"
@@ -56,10 +75,4 @@ export const ContactForm = ({ onSubmit }) => {
       <button type="submit">Add contact</button>
     </form>
   );
-};
-
-ContactForm.prototypes = {
-  name: PropTypes.string,
-  number: PropTypes.number,
-  onSubmit: PropTypes.func,
 };
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,21 +1,26 @@
-import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilter, getFilter } from '../../redux/slice';
+
 import './Filter.module.css';
 
-export const Filter = ({ filter, changeFilterInput }) => (
- 
-  <label>
-    <p>Find contacts by name</p>
-    <input
-      type="text"
-      name={filter}
-      onChange={changeFilterInput}
-      placeholder=""
-      
-    />
-  </label>
-);
+export const Filter = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(getFilter);
 
-Filter.propTypes = {
-  filter: PropTypes.string.isRequired,
-  changeFilterInput: PropTypes.func.isRequired,
+  const onChangeFilter = e => {
+    dispatch(setFilter(e.target.value));
+  };
+
+  return (
+    <label>
+      <p>Find contacts by name</p>
+      <input
+        type="text"
+        name="filter"
+        value={filter}
+        onChange={onChangeFilter}
+        placeholder=""
+      />
+    </label>
+  );
 };
